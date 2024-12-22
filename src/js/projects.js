a3e927e4589bcbb570c65bb035a18aa5054c9eee
Tetiana <tatiana.makarenko94@gmail.com>
2024-12-22 19:25:20 +0200

    Fixed errors and added js code

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,4 +1,17 @@
 // бібліотека swiper завантажена:
 import Swiper from 'swiper';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
