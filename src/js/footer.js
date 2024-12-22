58c0177661ef74fe842af6fb00b7e1018b1b7a81
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-12-22 18:36:11 +0200

    fotrv1

---
+++ src/js/footer.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("thankYouModal");
+    var closeButton = document.querySelector(".close-button");
+
+    document.getElementById("contactForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        modal.style.display = "flex";
+    });
+
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
