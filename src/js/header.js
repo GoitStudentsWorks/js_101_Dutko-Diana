2aced067006bd48eca4fde10e9aa316f508df2d7
veronikavatsyak <veronikateslya@ukr.net>
2024-12-22 21:17:27 -0500

    correcting css and js

--- src/js/header.js
+++ src/js/header.js
@@ -6,7 +6,7 @@ document.addEventListener('click', onDocumentClick);
 
 function onClick(event) {
   event.preventDefault();
-  menuList.classList.toggle('is-hidden');
+  menuList.classList.toggle('show');
   const links = document.querySelectorAll('.menu-list a');
   links.forEach(link => {
     link.addEventListener('click', handleClicks);
@@ -15,13 +15,14 @@ function onClick(event) {
 
 function onDocumentClick(event) {
   if (!menu.contains(event.target) && !menuList.contains(event.target)) {
-    menuList.classList.add('is-hidden');
+    menuList.classList.remove('show');
   }
 }
 
 function handleClicks(event) {
   event.preventDefault();
   const sectionId = event.target.getAttribute('href').substring(1);
+
   const section = document.getElementById(sectionId);
   if (section) {
     section.scrollIntoView({
@@ -29,5 +30,5 @@ function handleClicks(event) {
       block: 'start',
     });
   }
-  menuList.classList.add('is-hidden');
+  menuList.classList.remove('show');
 }
