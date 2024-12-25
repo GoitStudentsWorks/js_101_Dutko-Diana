6f87149d170d5a92f6f5afcb41f4c94656760856
Diana Dutko <dutkodiana2007@gmail.com>
2024-12-25 11:49:39 +0200

    try navigation

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -6,35 +6,35 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import axios from 'axios';
 
-
 const reviewContainer = document.getElementById('gallery-container');
 // запит на сервер
 async function fetchReviews() {
-   const response = await axios.get('https://portfolio-js.b.goit.study/api/reviews');
+  const response = await axios.get(
+    'https://portfolio-js.b.goit.study/api/reviews'
+  );
   return response.data;
 }
 
 async function reviews() {
-   try {
+  try {
     const review = await fetchReviews();
     createReviews(review);
-
-   } catch (error) {
-     reviewContainer.innerHTML = '<p class="error-message">Not found</p>'
-   return iziToast.error({
-        title: 'Error',
-        message: 'Not Found',
-        position: 'topRight',
-        backgroundColor: 'red',
-        theme: 'dark',
-        overlay: false,
-        titleColor: 'white',
-        messageColor: 'white',
-        overlayColor: 'rgba(0, 0, 0, 0.6)',
-      });
-    }
+  } catch (error) {
+    reviewContainer.innerHTML = '<p class="error-message">Not found</p>';
+    return iziToast.error({
+      title: 'Error',
+      message: 'Not Found',
+      position: 'topRight',
+      backgroundColor: 'red',
+      theme: 'dark',
+      overlay: false,
+      titleColor: 'white',
+      messageColor: 'white',
+      overlayColor: 'rgba(0, 0, 0, 0.6)',
+    });
+  }
 }
- 
+
 reviews();
 
 // формуємо картки
@@ -58,8 +58,8 @@ const swiper = new Swiper('.reviews-container.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 16,
   navigation: {
-    nextEl: '.btn-next',
-    prevEl: '.btn-prev',
+    nextEl: '.reviews-btn.btn-next',
+    prevEl: '.reviews-btn.btn-prev',
     disabledClass: 'swiper-button-disabled',
   },
   breakpoints: {
@@ -79,4 +79,3 @@ const swiper = new Swiper('.reviews-container.swiper-container', {
   },
   mousewheel: true,
 });
-
