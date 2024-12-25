b9cfb84ad9800df0207571cdaed7ffa8822e6216
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2024-12-25 14:48:44 +0200

    refactor covers.css and add js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,1 +1,23 @@
 //
+
+const target = document.querySelector('.covers-container');
+const covers = document.querySelectorAll('.marquee__line');
+
+const observerOptions = {
+  root: null, 
+  threshold: 0,
+};
+
+function onIntersection(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        [...covers].forEach(cover => cover.classList.add('is-active'));
+    } else {
+        [...covers].forEach(cover => cover.classList.remove('is-active'));
+    }
+  });
+}
+
+const observer = new IntersectionObserver(onIntersection, observerOptions);
+observer.observe(target);
+
